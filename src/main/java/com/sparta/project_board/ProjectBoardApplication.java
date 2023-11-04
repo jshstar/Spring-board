8ceebe1a957ac80d7f7ca5a788654f0292f2275d
jshstar <jshstar97@gmail.com>
2023-11-04 20:07:49 +0900

    init commit

---
+++ src/main/java/com/sparta/project_board/ProjectBoardApplication.java
@@ -0,0 +1,14 @@
+package com.sparta.project_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+public class ProjectBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectBoardApplication.class, args);
+    }
+
+}
